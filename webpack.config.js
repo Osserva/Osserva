ba70304ae8b7a94fdc7253c76d0d2e5db22afdb5
Lucienne Seidler <lucienneseidler@luciennes-mbp.lan>
2023-06-26 15:48:12 -0400

    ts config and webpack

---
+++ webpack.config.js
@@ -0,0 +1,54 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: './build/index.js',
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?/,
+        exclude: /node-modules/,
+        use: {
+          presets: [
+            ['@babel/preset-env', { targets: 'defaults' }],
+            ['@babel/preset-react', { targets: 'defaults' }],
+          ],
+        },
+      },
+      {
+        test: /\.tsx?/,
+        exclude: /node-modules/,
+        use: ['ts-loader'],
+      },
+      {
+        test: /\.s?css/,
+        exclude: [/node_modules/],
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.js', '.jsx', '.ts', '.tsx', '.gif', '.png', '.svg'],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './index.html',
+    }),
+  ],
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, './dist'),
+      publicPath: './dist',
+    },
+    compress: true,
+    port: 8080,
+    proxy: {
+      '*': 'http://localhost:3000',
+    },
+  },
+};
