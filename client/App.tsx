401f48f1fed31492d67493f10e7ab8c5b10b5c55
Lucienne Seidler <lucienneseidler@luciennes-mbp.lan>
2023-06-26 20:55:33 -0400

    landing pages for login, signup, error, and react routes
    Co-authored-by: kelsicw <kelsicw@users.noreply.github.com>

---
+++ client/App.tsx
@@ -0,0 +1,5 @@
+const App = () => {
+  console.log('hello, this is the app');
+};
+
+export default App;
---
+++ client/index.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { createRoot } from 'react-dom/client';
+import App from './App';
+import ErrorPage from './Error-Page';
+import './src/scss/App.scss';
+import Login from './Login';
+import Signup from './Signup';
+import path from 'path';
+
+const Main: React.FC = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<App />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/signup' element={<Signup />} />
+        <Route path='/error' element={<ErrorPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+const root = createRoot(document.getElementById('app'));
+
+root.render(
+  <React.StrictMode>
+    <Main />
+  </React.StrictMode>
+);
