b96304860fdc70611360ea99e1af2a51d40b2a45
Joseph-Caballero <123486138+Joseph-Caballero@users.noreply.github.com>
2023-06-28 13:07:48 -0400

    Reduced server count by 1

--- server/server.ts
+++ server/server.ts
@@ -13,12 +13,10 @@ import focusRouter from './routes/focusRouter';
 // Ports
 const PORT_1 = 4000;
 const PORT_2 = 4001;
-const PORT_3 = 4002;
 
 // Servers
 const app1: Express = express();
 const app2: Express = express();
-const app3: Express = express();
 
 // Body Parsers
 app1.use(express.json());
@@ -26,9 +24,6 @@ app1.use(express.urlencoded({ extended: true }));
 
 app2.use(express.json());
 app2.use(express.urlencoded({ extended: true }));
-
-app3.use(express.json());
-app3.use(express.urlencoded({ extended: true }));
 
 // Routers
 app1.use('/user', userRouter);
@@ -39,11 +34,6 @@ app2.use('/user', userRouter);
 app2.use('/entry', entryRouter)
 app2.use('/focus', focusRouter);
 
-app3.use('/user', userRouter);
-app3.use('/entry', entryRouter)
-app3.use('/focus', focusRouter);
-
-
 // 404 handlers
 app1.use('*', (req, res) => {
     res.status(404).send('Not Found');
@@ -51,11 +41,6 @@ app1.use('*', (req, res) => {
 app2.use('*', (req, res) => {
   res.status(404).send('Not Found');
 });
-
-app3.use('*', (req, res) => {
-  res.status(404).send('Not Found');
-});
-
 
 // Global Error Handlers
 app1.use(
@@ -93,25 +78,6 @@ app2.use(
   },
 );
 
-app3.use(
-  (
-    err: ErrorRequestHandler,
-    req: Request,
-    res: Response,
-    next: NextFunction,
-  ) => {
-    const defaultErr = {
-      log: 'Express error handler caught unknown middleware error',
-      status: 500,
-      message: { err: 'An error occurred' },
-    };
-    const errorObj = Object.assign({}, defaultErr, err);
-    console.log(errorObj.log);
-    return res.status(errorObj.status).json(errorObj.message);
-  },
-);
-
 // Listeners
 app1.listen(PORT_1, () => console.log('listening on port ', PORT_1));
 app2.listen(PORT_2, () => console.log('listening on port ', PORT_2));
-app3.listen(PORT_3, () => console.log('listening on port ', PORT_3));
