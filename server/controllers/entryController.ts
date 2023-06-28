cd05cdbdac4614ce6db31af60a3ee4d7044a7a4e
Eduardo Zayas <ezayasavila@gmail.com>
2023-06-28 12:29:18 -0400

    added getNotes middleware

--- server/controllers/entryController.ts
+++ server/controllers/entryController.ts
@@ -6,15 +6,17 @@ interface EntryController {
   getEntry: (req: Request, res: Response, next: NextFunction) => void;
   updateEntry: (req: Request, res: Response, next: NextFunction) => void;
   viewData: (req: Request, res: Response, next: NextFunction) => void;
+  addNotes: (req: Request, res: Response, next: NextFunction) => void;
+  getNotes: (req: Request, res: Response, next: NextFunction) => void;
 }
 
 const entryController: EntryController = {
   addEntry: async (req, res, next) => {
     console.log('entered create entry');
     try {
-      // update query text
+      // TODO: update query text to include notes???
       const query = 'INSERT INTO Focus_ratings (date, rating, focus_id) VALUES ($1, $2, $3);'
-      // update values
+      // TODO: remove req.body.userId
       const {date, rating, id} = req.body;
       const values = [date, rating, id];
       await db.query(query, values);
@@ -33,11 +35,14 @@ const entryController: EntryController = {
     console.log('entered get entry');
     try {
       // update query text
-      const query = 'SELECT * FROM Focus_ratings WHERE date = $1';
+      const query = 'SELECT fr.* FROM Focus_ratings fr JOIN Focuses f ON fr.focus_id = f.focus_id WHERE f.user_id = $1 AND fr.date = $2';
       // update values
-      const values = [req.body.date];
+      const { date } = req.body;
+      // TODO: update userId
+      const values = [1, date];
       const entry = await db.query(query, values);
-      res.locals.entry = entry.rows;
+      console.log(entry.rows)
+      res.locals.ratings = entry.rows;
       
       return next();
     } catch (error) {
@@ -53,8 +58,8 @@ const entryController: EntryController = {
     try {
       // add query text
       const query = '(user_id, date, entryData)';
-      // update values
-      const values = [req.params.userId, req.body.entry];
+      // TODO: remove req.body.userId
+      const values = [req.body.userId, req.body.entry];
       const entry = await db.query(query, values);
       res.locals.entry = entry.rows;
     } catch (error) {
@@ -65,12 +70,47 @@ const entryController: EntryController = {
       });
     }
   },
+  
   viewData: async (req, res, next) => {
     try {
       const query = 'SELECT * FROM Focus_ratings;';
       const result = await db.query(query);
       console.log('Data:', result.rows);
       res.locals.data = result.rows;
+
+      return next();
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
+  },
+
+  addNotes: async (req, res, next) => {
+    console.log('entered addNotes');
+    try {
+      // const query = 'INSERT INTO Notes (date, contents) VALUES ($1, $2);'
+      const query = 'INSERT INTO Notes (contents, user_id) VALUES ($1, $2);'
+      const { date, contents } = req.body;
+      const user_id = 1
+      // const values = [date, contents];
+      const values = [contents, user_id];
+      await db.query(query, values);
+      
+      return next();
+    } catch (error) {
+      return next({
+        log: `Error in createEntry controller method: ${error}`,
+        status: 400,
+        message: 'Error while creating new entry',
+      });
+    }
+  },
+
+  getNotes: async (req, res, next) => {
+    try {
+      const query = 'SELECT * FROM Notes;';
+      const result = await db.query(query);
+      console.log('Data:', result.rows);
+      res.locals.notes = result.rows;
 
       return next();
     } catch (error) {
--- server/routes/entryRouter.ts
+++ server/routes/entryRouter.ts
@@ -4,16 +4,32 @@ import authController from '../controllers/authController';
 
 const entryRouter = express.Router();
 
-entryRouter.get('/', entryController.getEntry, (req, res) => {
-    return res.status(200).send(res.locals.entry);
+entryRouter.post('/ratings', entryController.getEntry, (req, res) => {
+    return res.status(200).send(res.locals.ratings);
+});
+
+entryRouter.post('/', entryController.getEntry, entryController.getNotes, (req, res) => {
+    return res.status(200).send({ratings: res.locals.ratings, notes: res.locals.notes});
 });
 
 entryRouter.post('/add', entryController.addEntry, (req, res) => {
     return res.sendStatus(200);
 });
 
+// notes:
+
+entryRouter.post('/addNotes', entryController.addNotes, (req, res) => {
+    return res.sendStatus(200);
+});
+
+entryRouter.get('/getNotes', entryController.getNotes, (req, res) => {
+    return res.status(200).send(res.locals.data);
+});
+
+// notes and ratings:
 
 entryRouter.get('/viewData', entryController.viewData, (req, res) => {
     return res.status(200).send(res.locals.data);
 });
 
+export default entryRouter;
